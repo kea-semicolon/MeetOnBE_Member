328df6bdc92e314dae3a745e3ce2ffd4a0c32140
Lee <lych0918@gmail.com>
2024-03-27 12:22:20 +0900

    [REFACTOR] 카카오 로그인 인가 코드 Json Body로 수정

--- src/main/java/semicolon/MeetOn/domain/admin/api/KaKaoController.java
+++ src/main/java/semicolon/MeetOn/domain/admin/api/KaKaoController.java
@@ -2,6 +2,7 @@ package semicolon.MeetOn.domain.admin.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import semicolon.MeetOn.domain.admin.OAuth.OAuthLoginParams;
@@ -10,6 +11,8 @@ import semicolon.MeetOn.domain.admin.application.AdminService;
 import semicolon.MeetOn.domain.admin.dto.AdminDto;
 import semicolon.MeetOn.domain.admin.dto.AuthToken;
 
+@Slf4j
+@CrossOrigin("http://localhost:3000")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("")
@@ -18,8 +21,12 @@ public class KaKaoController {
     private final AdminService adminService;
 
     @PostMapping("/oauth/callback/kakao")
-    public ResponseEntity<AuthToken> callBack(@RequestParam String code) {
-        OAuthLoginParams oAuthLoginParams = new KakaoLoginParams(code);
-        return ResponseEntity.ok(adminService.login(oAuthLoginParams));
+    public ResponseEntity<AuthToken> callBack(@RequestBody KakaoLoginParams authorizationCode) {
+        log.info("code");
+        log.info("code={}", authorizationCode.getAuthorizationCode());
+        AuthToken token = adminService.login(authorizationCode);
+        log.info("accessToken={}", token.getAccessToken());
+        log.info("refreshToken={}", token.getRefreshToken());
+        return ResponseEntity.ok(token);
     }
 }
