bc0a995be765314ae2f2be2ab77f8b0d2fe419d3
Lee <lych0918@gachon.ac.kr>
2024-04-12 09:14:03 +0900

    [ADD] Swagger 베포

--- src/main/java/semicolon/MeetOn/global/swagger/SwaggerConfig.java
+++ src/main/java/semicolon/MeetOn/global/swagger/SwaggerConfig.java
@@ -7,29 +7,47 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
+@Slf4j
 @OpenAPIDefinition(info = @Info(
-        title = "MoodMate API",
-        description = "MoodMate : API 명세서",
+        title = "MeetOn Member API",
+        description = "MeetOn : API 명세서",
         version = "v1.0.0"))
 @Configuration
 public class SwaggerConfig {
-    @Bean
-    public OpenAPI openApi() {
-        String jwt = "JWT";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT");
-        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
-                .name(jwt)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-        );
+//    @Bean
+//    public OpenAPI openApi() {
+//        String jwt = "JWT";
+//        SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT");
+//        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+//                .name(jwt)
+//                .type(SecurityScheme.Type.HTTP)
+//                .scheme("bearer")
+//                .bearerFormat("JWT")
+//        );
+//
+//        return new OpenAPI()
+//                .addServersItem(new Server().url("/"))
+//                .addSecurityItem(securityRequirement)
+//                .components(components);
+//    }
 
+    @Bean
+    public OpenAPI customOpenApi(@Value("${openapi.service.url}") String url) {
+        log.info("serverUrl={}", url);
         return new OpenAPI()
-                .addServersItem(new Server().url("/"))
-                .addSecurityItem(securityRequirement)
-                .components(components);
+                .servers(List.of(new Server().url(url)))
+                .components(new Components().addSecuritySchemes("Bearer",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .info(new io.swagger.v3.oas.models.info.Info().title("KEA Project-SemiColon")
+                        .description("Member 관련 API")
+                        .version("v0 0.1"));
     }
 }
