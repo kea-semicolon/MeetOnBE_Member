678adb98c9cf82c0190480949d7d58acb34a1fef
ManchanTime <127479677+ManchanTime@users.noreply.github.com>
2024-04-24 01:18:19 +0900

    reset

--- src/main/java/semicolon/MeetOn/global/config/WebConfig.java
+++ src/main/java/semicolon/MeetOn/global/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
                 .allowCredentials(true)
                 .maxAge(3000);
     }
