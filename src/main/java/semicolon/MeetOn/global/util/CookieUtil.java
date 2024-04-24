847713af0807eaa2ff5e1b93e1ae41e353e660ea
Lee <lych0918@gachon.ac.kr>
2024-04-24 11:44:41 +0900

    Reset

--- src/main/java/semicolon/MeetOn/global/util/CookieUtil.java
+++ src/main/java/semicolon/MeetOn/global/util/CookieUtil.java
@@ -23,6 +23,7 @@ public class CookieUtil {
         cookie.setSecure(false);
         cookie.setPath("/");
         cookie.setMaxAge(1000 * 60 * 60 * 24 * 7);
+        cookie.setDomain("localhost");
         response.addCookie(cookie);
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Origin",  "http://localhost");
