eed83227d60dbe30be0d1de36168b8e0f1507d51
Lee <lych0918@gachon.ac.kr>
2024-04-24 12:24:00 +0900

    Reset

--- src/main/java/semicolon/MeetOn/global/util/CookieUtil.java
+++ src/main/java/semicolon/MeetOn/global/util/CookieUtil.java
@@ -20,10 +20,9 @@ public class CookieUtil {
         Cookie cookie = new Cookie(cookieName, cookieValue);
         //쿠키 속성 설정
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setSecure(false);
         cookie.setPath("/");
         cookie.setMaxAge(1000 * 60 * 60 * 24 * 7);
-        cookie.setDomain(".ngrok-free.app");
         response.addCookie(cookie);
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Origin",  "http://localhost");
