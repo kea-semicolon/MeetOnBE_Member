12369c9a672314622d387c9be16542a378c0675e
ManchanTime <lych0918@gachon.ac.kr>
2024-05-13 12:47:13 +0900

    fix: WebConfig allowedOrigins add VM ip address

--- src/main/java/semicolon/MeetOn/global/config/WebConfig.java
+++ src/main/java/semicolon/MeetOn/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://172.16.212.76:3000")
                 .allowedMethods("*")
                 .allowCredentials(true)
                 .maxAge(3000);
